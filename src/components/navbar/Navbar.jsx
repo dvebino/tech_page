52eaa6d94009358a93382a17ec1ea06969bf7839
dvebino <dhuang2495@gmail.com>
2023-01-05 15:57:34 -0500

    navbar dropdown menu done -> moving onto header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,15 @@ import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 //BEM -> Block Element Modifier Naming 
-
+const Menu = () => (
+  <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#wgpt3'>What's GPT?</a></p>
+    <p><a href='#possibility'>Open AI</a></p>
+    <p><a href='#features'>Case Studies</a></p>
+    <p><a href='#blog'>Documentation</a></p>
+  </>
+)
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false)
@@ -18,11 +26,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className='gpt3__navbar-links_container'>
-          <p><a href='#home'>Home</a></p>
-          <p><a href='#wgpt3'>What's GPT?</a></p>
-          <p><a href='#possibility'>Open AI</a></p>
-          <p><a href='#features'>Case Studies</a></p>
-          <p><a href='#blog'>Documentation</a></p>
+          <Menu />
         </div>
       </div>
 
@@ -30,7 +34,7 @@ const Navbar = () => {
         <p>Sign In</p>
         <button type='button'>Sign Up</button>
       </div>
-
+git 
       <div className='gpt3__navbar-menu'>
         { toggleMenu ? 
           <RiCloseLine color="fff" size = {27} onClick={()=>setToggleMenu(false)}/>  
@@ -40,7 +44,11 @@ const Navbar = () => {
         { toggleMenu && (
           <div className='gpt3__navbar-menu_container scale-up-center'>
             <div className='gpt3__navbar-menu_container-links'>
-
+              <Menu />
+              <div className='gpt3__navbar-menu_container-links-sign'>
+                <p>Sign In</p>
+                <button type='button'>Sign Up</button>
+              </div>
             </div>
           </div>
         )
