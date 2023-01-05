9d6b87de7e7b4bdbb5166a15819e155c86502bcf
dvebino <dhuang2495@gmail.com>
2023-01-05 15:50:15 -0500

    created dropdown menu, adding new menu component for links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, {useState} from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 
 
@@ -7,7 +7,10 @@ import './navbar.css';
 
 //BEM -> Block Element Modifier Naming 
 
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
@@ -24,8 +27,25 @@ const Navbar = () => {
       </div>
 
       <div className='gpt3__navbar-sign'>
-       <p>Sign In</p>
-       <button type='button'>Sign Up</button>
+        <p>Sign In</p>
+        <button type='button'>Sign Up</button>
+      </div>
+
+      <div className='gpt3__navbar-menu'>
+        { toggleMenu ? 
+          <RiCloseLine color="fff" size = {27} onClick={()=>setToggleMenu(false)}/>  
+          :
+          <RiMenu3Line color='fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+        { toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+
+            </div>
+          </div>
+        )
+
+        } 
       </div>
     </div>
   )
