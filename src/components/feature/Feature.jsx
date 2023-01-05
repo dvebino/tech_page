0d13e4b58a2f2ef607580e997cdcc3c959aa6224
dvebino <dhuang2495@gmail.com>
2023-01-05 18:04:49 -0500

    styled gpt3, moving to feature.jsx

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({title}) => {
   return (
-    <div>Feature</div>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+    </div>
   )
 }
 
