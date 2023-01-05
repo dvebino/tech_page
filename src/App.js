82d041b7a59f2782c2b4ed777574724915d0e984
dvebino <dhuang2495@gmail.com>
2023-01-05 14:39:35 -0500

    importing components/containers into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './app.css';
 
 const App = () => {
   return (
-    <div>
-        <h1>Chef_js</h1>
+    <div className='App'>
+        <div className='gradient_bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
