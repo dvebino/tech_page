dbf12cd523bf58ddb397f12d77b897d9a8553e07
dvebino <dhuang2495@gmail.com>
2023-01-05 19:30:27 -0500

    working on possibility container

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,8 +10,8 @@ const Menu = () => (
   <>
     <p><a href='#home'>Home</a></p>
     <p><a href='#wgpt3'>What's GPT?</a></p>
-    <p><a href='#possibility'>Open AI</a></p>
     <p><a href='#features'>Case Studies</a></p>
+    <p><a href='#possibility'>Open AI</a></p>
     <p><a href='#blog'>Documentation</a></p>
   </>
 )
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+
+import possibilityImage from '../../assets/possibility.png'
 import './possibility.css'
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibilityImage" />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access</h4>
+        <h1 className='gradient__text'>The possibilities are endless</h1>
+        <p>The potential of artificial intelligence (AI) is vast and varied, with the ability to revolutionize virtually every industry and aspect of our lives.</p>
+        <h4>Request Early Access</h4>
+      </div>
+    </div>
   )
 }
 
